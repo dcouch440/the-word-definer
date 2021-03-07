c49e83357fc38651e19aeff6dbf96843da39909f
dcouch440 <dcouch440@gmail.com>
2021-03-06 22:56:03 -0800

    change test format > before each change

--- spec/definition_integration_spec.rb
+++ spec/definition_integration_spec.rb
@@ -6,36 +6,23 @@ set :show_exceptions, false
 
 describe "create a definition path", {:type => :feature} do
 
-  before :each do
+  scenario "should be saved after a user submits one" do
     visit "/words"
     fill_in "word_input", :with => "Zap"
     click_on "New Word"
     visit "/word/1"
-  end
-
-  scenario "should be saved after a user submits one" do
     fill_in "definition_input", :with => "Electrocute"
     click_on "Enter Definition"
-    page.has_content? "Electrocute"
+    expect(page).to have_content "Electrocute"
   end
 
   scenario "should be able to be changed the definition" do
+    visit "/word/1"
     click_on "Change definition"
     fill_in "update_definition", :with => "Electrocutes a person"
     click_on "Update"
     visit "word/1"
-    page.has_content? "Electrocutes a person"
-  end
-
-  scenario "can update definitions multiple times" do
-    (1..5).each do |iteration|
-      click_on "Change definition"
-      fill_in "update_definition", :with => "UPDATE_#{iteration}"
-      click_on "Update"
-      visit "word/1"
-    end
-    visit "word/1"
-    page.has_content? "UPDATE_5"
+    expect(page).to have_content "Electrocutes a person"
   end
 
 end
--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -8,50 +8,38 @@ describe "create a word path", {:type => :feature} do
 
   before :each do
     visit "/words"
-    fill_in "word_input", :with => "Zap"
-    click_on "New Word"
   end
 
   scenario "creates an word and lists it on the page" do
-    page.has_content? "Zap"
+    fill_in "word_input", :with => "Zap"
+    click_on "New Word"
+    expect(page).to have_content "Zap"
   end
 
   scenario "creates a link to fallow that words id in the link" do
-    page.has_link? 'word/1'
+    expect(page).to have_link href: 'word/1'
   end
 
   scenario "deletes the word when told too" do
-    visit "word/1/change"
+    visit "/word/1/change"
     click_on "Delete Word"
-    page.has_no_link? 'word/1'
+    expect(page).to have_no_link href: 'word/1'
   end
 
   scenario "adds more than one word" do
-    (1..5).each do |int|
-      fill_in "word_input", :with => "TEST_#{int}"
+    (1..4).each do |int|
+      fill_in "word_input", :with => "TEST_LOOP_1_#{int}"
       click_on "New Word"
     end
-    page.has_link? "word/6"
+    expect(page).to have_link href: 'word/5'
   end
 
   scenario "uses global index to index words in ascending order" do
     (1..5).each do |int|
-      fill_in "word_input", :with => "TEST_#{int}"
+      fill_in "word_input", :with => "TEST_LOOP_2_#{int}"
       click_on "New Word"
     end
-    page.has_content? "TEST_5"
-  end
-
-  scenario "can remove words multiple times without error" do
-    (1..10).each do |int|
-      fill_in "word_input", :with => "TEST_#{int}"
-      click_on "New Word"
-    end
-    [10, 5, 2].each do |int|
-      visit "word/#{int}/change"
-      click_on "Delete Word"
-    end
-    page.has_no_content? ( 'TEST_10' || 'TEST_5' || 'TEST_2' )
+    expect(page).to have_content "TEST_LOOP_2_5"
   end
 
 end
